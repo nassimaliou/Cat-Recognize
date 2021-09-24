fc67e0648d703fac9f1a57cb9901f9d97500080b
Nassim069 <im_aliousalah@esi.dz>
2021-09-24 20:07:14 +0100

    consulting the training set

--- meow.py
+++ meow.py
@@ -15,12 +15,17 @@ from scipy import ndimage
 #%autoreload 2
 
 
+"""
+    training set : 209 cat and non cat pic
+    testing set : 
+"""
+
 def load_dataset():
-    with h5py.File('datasets/train_catvnoncat.h5', "r") as train_dataset:
+    with h5py.File('datasets/catvnoncat/train_catvnoncat.h5', "r") as train_dataset:
         train_set_x_orig = np.array(train_dataset["train_set_x"][:])
         train_set_y_orig = np.array(train_dataset["train_set_y"][:])
 
-    with h5py.File('datasets/test_catvnoncat.h5', "r") as test_dataset:
+    with h5py.File('datasets/catvnoncat/test_catvnoncat.h5', "r") as test_dataset:
         test_set_x_orig = np.array(test_dataset["test_set_x"][:])
         test_set_y_orig = np.array(test_dataset["test_set_y"][:])
         classes = np.array(test_dataset["list_classes"][:])
@@ -32,3 +37,10 @@ def load_dataset():
 
 
 train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+
+# Run this code to consult the training set (0 <= index <= 208)
+#"""
+index = 88
+plt.imshow(train_set_x_orig[index])
+print ("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") +  "' picture.")
+#"""
