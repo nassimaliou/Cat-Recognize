fcab40055bb49bb86d5b6e567750826a5538f82e
Nassim069 <im_aliousalah@esi.dz>
2021-09-24 19:47:15 +0100

    load_dataset function

--- meow.py
+++ meow.py
@@ -5,5 +5,30 @@ import h5py
 import scipy
 from PIL import Image
 from scipy import ndimage
-from lr_utils import load_dataset
-from public_tests import *
+#from lr_utils import load_dataset
+#from public_tests import *
+
+
+
+#%matplotlib inline
+#%load_ext autoreload
+#%autoreload 2
+
+
+def load_dataset():
+    with h5py.File('datasets/train_catvnoncat.h5', "r") as train_dataset:
+        train_set_x_orig = np.array(train_dataset["train_set_x"][:])
+        train_set_y_orig = np.array(train_dataset["train_set_y"][:])
+
+    with h5py.File('datasets/test_catvnoncat.h5', "r") as test_dataset:
+        test_set_x_orig = np.array(test_dataset["test_set_x"][:])
+        test_set_y_orig = np.array(test_dataset["test_set_y"][:])
+        classes = np.array(test_dataset["list_classes"][:])
+
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+
+
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
